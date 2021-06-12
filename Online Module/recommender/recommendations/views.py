ce4015998202d711366e4232cb4246dd345621f3
AnjithPaul <65152866+AnjithPaul@users.noreply.github.com>
2021-06-12 06:06:30 +0530

    fix userProfile null error

--- Online Module/recommender/recommendations/views.py
+++ Online Module/recommender/recommendations/views.py
@@ -52,11 +52,13 @@ def recommendation(request):
     courses_collab_final = []
     courses_hybrid_final = []
 
-
-
     ###CONTENT BASED###
 
-    user_profile = UserProfiles.objects.get(user=request.user)
+    if UserProfiles.objects.filter(user=request.user).exists():
+        user_profile = UserProfiles.objects.get(user=request.user)
+    else:
+        return render(request, "debug.html", {"result": "Complete user profile to get recommendations"})
+
     topics_str = user_profile.topics
     user_topics = [x.strip() for x in topics_str.split(',')]
 
@@ -76,9 +78,7 @@ def recommendation(request):
 
     courses_content = Course.objects.filter(pk__in=[x for x in content_probs_sorted])
 
-
-###COLLABORATIVE FILTERING###
-
+    ###COLLABORATIVE FILTERING###
 
     u1_likes = Rating.objects.filter(user=request.user, rating="1")
     u1_dislikes = Rating.objects.filter(user=request.user, rating="-1")
@@ -93,22 +93,25 @@ def recommendation(request):
             other_likes[user.pk] = Rating.objects.filter(user=user, rating="1")
             other_dislikes[user.pk] = Rating.objects.filter(user=user, rating="-1")
 
-            other_liked_courses[user.pk] = Course.objects.filter(pk__in=other_likes[user.pk].values_list('course', flat=True))
-            other_disliked_courses[user.pk] = Course.objects.filter(pk__in=other_dislikes[user.pk].values_list('course', flat=True))
+            other_liked_courses[user.pk] = Course.objects.filter(
+                pk__in=other_likes[user.pk].values_list('course', flat=True))
+            other_disliked_courses[user.pk] = Course.objects.filter(
+                pk__in=other_dislikes[user.pk].values_list('course', flat=True))
 
             common_likes = list(set(user_liked_courses).intersection(set(other_liked_courses[user.pk])))
             common_dislikes = list(set(user_disliked_courses).intersection(set(other_disliked_courses[user.pk])))
             opposite_rating1 = list(set(user_liked_courses).intersection(set(other_disliked_courses[user.pk])))
             opposite_rating2 = list(set(user_disliked_courses).intersection(set(other_liked_courses[user.pk])))
 
-            lst = [user_liked_courses, user_disliked_courses, other_liked_courses[user.pk], other_disliked_courses[user.pk]]
+            lst = [user_liked_courses, user_disliked_courses, other_liked_courses[user.pk],
+                   other_disliked_courses[user.pk]]
             total = list(set().union(*lst))
 
             if len(total) != 0:
-                similarity[user.pk] = (len(common_likes) + len(common_dislikes) - len(opposite_rating1) - len(opposite_rating2)) / len(total)
+                similarity[user.pk] = (len(common_likes) + len(common_dislikes) - len(opposite_rating1) - len(
+                    opposite_rating2)) / len(total)
             else:
                 similarity[user.pk] = 0
-
 
     ratings = Rating.objects.all()
 
@@ -118,12 +121,10 @@ def recommendation(request):
         if rating.course.pk not in course_dislikes.keys():
             course_dislikes[rating.course.pk] = []
 
-
         if rating.rating == 1:
             course_likes[rating.course.pk].append(rating.user.pk)
         if rating.rating == -1:
             course_dislikes[rating.course.pk].append(rating.user.pk)
-
 
     for c in courses:
         if c.pk not in course_likes.keys():
@@ -135,12 +136,12 @@ def recommendation(request):
         similarity_sum_disliked = 0
 
         for i in course_likes[c.pk]:
-            if (i==request.user.pk):
+            if (i == request.user.pk):
                 continue
             similarity_sum_liked += similarity[i]
 
         for i in course_dislikes[c.pk]:
-            if (i==request.user.pk):
+            if (i == request.user.pk):
                 continue
             similarity_sum_disliked += similarity[i]
 
@@ -162,10 +163,7 @@ def recommendation(request):
 
         courses_collab = Course.objects.filter(pk__in=[x for x in collab_probs_sorted])
 
-
-
-###HYBRID###
-
+    ###HYBRID###
 
     for co in courses:
         hybrid_probs[co.pk] = content_probs[co.pk] * collab_probs[co.pk]
@@ -178,19 +176,16 @@ def recommendation(request):
 
     courses_hybrid = Course.objects.filter(pk__in=[x for x in hybrid_probs_sorted])
 
-
-###POST-PROCESSING###
+    ###POST-PROCESSING###
 
     hybrid_list = []
     content_list = []
     collab_list = []
 
-
     for i in hybrid_probs_sorted:
         if Rating.objects.filter(user=request.user, course=Course.objects.get(pk=i)).exists():
             continue
         hybrid_list.append(i)
-
 
     for i in content_probs_sorted:
         if Rating.objects.filter(user=request.user, course=Course.objects.get(pk=i)).exists():
@@ -208,14 +203,12 @@ def recommendation(request):
             continue
         collab_list.append(i)
 
-
     courses_content_final = Course.objects.filter(pk__in=[x for x in content_list])
     courses_collab_final = Course.objects.filter(pk__in=[x for x in collab_list])
     courses_hybrid_final = Course.objects.filter(pk__in=[x for x in hybrid_list])
 
-
-
     #    d_intersection = list(set(user_topics).intersection(set(course_topics[10])))
     #    d_prob = len(d_intersection)/len(course_topics[10])
     #    d_items = user_topics
-    return render(request, "recommendation.html", {"hybrid":courses_hybrid_final,"content":courses_content_final, "collab":courses_collab_final})
+    return render(request, "recommendation.html",
+                  {"hybrid": courses_hybrid_final, "content": courses_content_final, "collab": courses_collab_final})
