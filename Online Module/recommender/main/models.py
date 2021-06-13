17d5ff18c9fcdb53cb6834e84cb5cee99edeb0bb
Anjith Paul <65152866+AnjithPaul@users.noreply.github.com>
2021-06-13 13:16:03 +0530

    Delete Online Module directory

--- Online Module/recommender/main/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-
-# Create your models here.
-
-class UserProfiles(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    topics = models.CharField(max_length=250)
